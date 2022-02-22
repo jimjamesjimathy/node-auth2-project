3b27e2b8ffc0a2128457a411331f085c2ccdeb2a
James Kirkwood <85250373+jimjamesjimathy@users.noreply.github.com>
2022-02-22 15:08:40 -0500

    initial commit. I think I have to users-model correct.

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,6 +18,11 @@ function find() {
       }
     ]
    */
+  return db('users as u')
+    .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'role_name')
+
+
 }
 
 function findBy(filter) {
@@ -34,6 +39,10 @@ function findBy(filter) {
       }
     ]
    */
+  return db('users as u')
+    .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username','password', 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +56,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+  return db('users as u')
+    .leftJoin('roles as r', 'u.role_id', 'r.role_id')
+    .select('user_id', 'username', 'role_name')
+    .where('user_id', user_id).first()
 }
 
 /**
